dee56b4fcd92fec6c76fa505e36086c8edbc91d5
heyanjiao <he.y@ctw.inc>
2025-09-18 13:55:19 +0800

    feat: add react demo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ display: 'grid', placeItems: 'center', height: '100vh', fontSize: '24px' }}>
+      Pages 部署测试 OK
+    </div>
+  )
+}
+
+export default App
